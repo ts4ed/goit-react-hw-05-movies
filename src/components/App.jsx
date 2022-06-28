f929c8508ba3c39689b5e5c82ba6f43592d3fbdd
ts4ed <103256726+ts4ed@users.noreply.github.com>
2022-06-28 20:07:52 +0300

    update

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,12 +1,37 @@
 import { Suspense } from 'react';
 import { Routes, Route, BrowserRouter } from 'react-router-dom';
-import { Home } from './Home';
-import { Movies } from './Movies';
-import { NotFounder } from './NotFounder';
-import { MovieDetails } from './MovieDetails';
-import { MovieDetailsCast } from './MovieDetails/MovieDetailsCast';
-import { MovieDetailsReviews } from './MovieDetails/MovieDetailsReviews';
-import { NavLinks } from './NavLinks';
+// import { Home } from './Home';
+// import { Movies } from './Movies';
+// import { NotFounder } from './NotFounder';
+// import { MovieDetails } from './MovieDetails';
+// import { MovieDetailsCast } from './MovieDetails/MovieDetailsCast';
+// import { MovieDetailsReviews } from './MovieDetails/MovieDetailsReviews';
+// import { NavLinks } from './NavLinks';
+
+const Home = lazy(() => import('./Home' /* webpackChunkName: "home-page" */));
+
+const Movies = lazy(() =>
+  import('./Movies' /* webpackChunkName: "movies-page" */)
+);
+
+const NotFound = lazy(() =>
+  import('./NotFounder' /* webpackChunkName: "notFound-page" */)
+);
+
+const MovieDetailsCast = lazy(() =>
+  import(
+    './MovieDetails/MovieDetailsCast' /* webpackChunkName: "movieDetailsCast-page" */
+  )
+);
+const MovieDetailsReviews = lazy(() =>
+  import(
+    './MovieDetails/MovieDetailsReviews' /* webpackChunkName: "movieDetailsReviews-page" */
+  )
+);
+
+const NavLinks = lazy(() =>
+  import('./NavLinks' /* webpackChunkName: "navLinks-page" */)
+);
 
 export const App = () => {
   return (
