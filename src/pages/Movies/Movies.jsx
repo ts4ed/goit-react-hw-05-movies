c80dcca5b1195ae1ee394f79c9e8733a128d8e72
ts4ed <103256726+ts4ed@users.noreply.github.com>
2022-07-10 19:50:36 +0300

    update

--- src/pages/Movies/Movies.jsx
+++ src/pages/Movies/Movies.jsx
@@ -1,9 +1,11 @@
 import { SearchMovies } from 'components/SearchMovies/SearchMovies';
 import { useLocation, Outlet } from 'react-router-dom';
+import s from './Movies.module.css';
+
 const Movies = () => {
   const location = useLocation();
   return (
-    <div>
+    <div className={s.container}>
       {location.pathname === '/movies' && <SearchMovies />}
       <Outlet />
     </div>
